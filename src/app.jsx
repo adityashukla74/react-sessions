304bc67352f66a6377f71331681d237f3a7d3b0f
Aditya Shukla <adityashukla74@gmail.com>
2023-06-19 19:58:19 +0530

    refactored jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,17 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import Pet from "./pet";
+import Pet from "./Pet";
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>Adopt Me!</h1>
-      <Pet name='Luna' animal='dog' breed='Havanese' />
-      <Pet name='Pepper' animal='bird' breed='Cockateil' />
-      <Pet name='Kiri' animal='cat' breed='Mixed' />
-    </>
-  )
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-
 root.render(<App />);
